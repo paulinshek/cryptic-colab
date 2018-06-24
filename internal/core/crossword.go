dbc9fb3091d973f686b44a3c715acab62c4be00f
Martha Anne <martha@carbonvisuals.com>
2018-06-24 20:16:38 +0100

    Add struct for Solution

--- internal/core/crossword.go
+++ internal/core/crossword.go
@@ -12,11 +12,17 @@ type Clue struct {
 	signature string
 	clueText string
 	clueNumber int
-	clueId int
+	id int
 }
 
 type Crossword struct {
 	width, height int
 	clues []Clue
 	id int
+}
+
+type Solution {
+	id int
+	crosswordId int
+	inputLetters map[Coordinate]rune
 }
