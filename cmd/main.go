d3a936266ded1affdd76d893d5458ccfb82d41e0
Martha Anne <miss.martha.anne@googlemail.com>
2020-04-17 14:49:50 +0100

    set up very basic api endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,49 @@
 package main
 
 import (
-	"github.com/paulinshek/cryptic-colab/internal/pkg/core"
-	"github.com/paulinshek/cryptic-colab/internal/pkg/dataaccess"
-	"github.com/paulinshek/cryptic-colab/internal/pkg/io"
+	"fmt"
+	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	h := http.NewServeMux()
-	crossword, solution := initialState()
-
-	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		crosswordWriter, _ := io.GetCrosswordWriter()
-		err := crosswordWriter.Write(crossword, solution, w)
-		if (err != nil) {
-			
-		}
-	})
-
-	http.ListenAndServe(":8080", h)
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
 }
 
-func initialState() (core.Crossword, core.Solution) {
-	crosswordRepository, _ := dataaccess.GetCrosswordRepository()
-	crossword, _ := crosswordRepository.Get(1)
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// func main() {
+// 	h := http.NewServeMux()
+// 	crossword, solution := initialState()
+
+// 	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		crosswordWriter, _ := io.GetCrosswordWriter()
+// 		err := crosswordWriter.Write(crossword, solution, w)
+// 		if (err != nil) {
+			
+// 		}
+// 	})
+
+// 	http.ListenAndServe(":8080", h)
+// }
+
+// func initialState() (core.Crossword, core.Solution) {
+// 	crosswordRepository, _ := dataaccess.GetCrosswordRepository()
+// 	crossword, _ := crosswordRepository.Get(1)
 	
 
-	mySolution := core.Solution {
-		Id: 1,
-		CrosswordId: crossword.Id,
-		InputChars: make(map[core.Coordinate]rune),
-	}
-	mySolution.InputChars[core.Coordinate{0,0}] = 'c'
+// 	mySolution := core.Solution {
+// 		Id: 1,
+// 		CrosswordId: crossword.Id,
+// 		InputChars: make(map[core.Coordinate]rune),
+// 	}
+// 	mySolution.InputChars[core.Coordinate{0,0}] = 'c'
 
-	return crossword, mySolution
-}
+// 	return crossword, mySolution
+// }
 
