9afe7ab8b02cbb102c5ae5d9db45e93541508023
Martha Anne <miss.martha.anne@googlemail.com>
2020-06-03 16:41:29 +0100

    add test file skeleton for crosswordGridService

---
+++ web/tests/services/crosswordGridService.test.ts
@@ -0,0 +1,12 @@
+import "crosswordGridService" from "./../../../src/services/crosswordGridService"
+import expect from 'expect'
+
+describe("crosswordGridService", () => {
+    describe("getIsInputAtCoordinate", () => {
+        it("returns true if coordinate is the start coordinate of a provided clue", () => {})
+        it("returns true if coordinate is within the boundary of an across clue", () => {})
+        it("returns true if coordinate is within the boundary of a down clue", () => {})
+        it("returns false if coordinate is not within the boundary of any clue", () => {})
+    })
+})
+
