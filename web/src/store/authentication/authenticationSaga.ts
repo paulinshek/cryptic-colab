c3c92f6276458ed56a5261855762ec0c71a0829e
Martha Anne <miss.martha.anne@googlemail.com>
2020-06-07 13:43:37 +0100

    request made to get authentication url by authentication saga

--- web/src/store/authentication/authenticationSaga.ts
+++ web/src/store/authentication/authenticationSaga.ts
@@ -1,1 +1,25 @@
-export const j = 1
+import { takeLatest, call, put } from "redux-saga/effects";
+
+import { Action } from "typesafe-actions";
+import axios from "axios";
+
+import * as AuthenticationTypes from "./authenticationTypes";
+import {  } from "./authenticationActions";
+
+function* getAuthenticationUrl() {
+  const url =
+    "/api/getauthenticationurl"
+
+  try {
+    const response = yield call(() => axios.get(url));
+    console.log(response.data);
+    // put success
+  } catch (error) {
+    console.log(error);
+    // put error
+  }
+}
+
+export default function* () {
+  yield takeLatest(AuthenticationTypes.AuthenticationActionTypes.REQUEST_GET_AUTHENTICATION_URL, getAuthenticationUrl);
+}
--- web/src/store/rootSaga.ts
+++ web/src/store/rootSaga.ts
@@ -1,6 +1,7 @@
 import { all, fork } from "redux-saga/effects";
 import crosswordSaga from "./crossword/crosswordSaga";
+import authenticationSaga from "./authentication/authenticationSaga";
 
 export default function* rootSaga() {
-  yield all([fork(crosswordSaga)]);
+  yield all([fork(crosswordSaga), fork(authenticationSaga)]);
 }
